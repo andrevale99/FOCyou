907bfb551d0bc6fc3005510bb03543cb7b3e0cd7
andrevale99 <andrefvale@gmail.com>
2026-05-31 17:41:29 -0300

    Update

---
+++ FOCyou/lib/tim1pwm/tim1pwm.h
@@ -0,0 +1,7 @@
+#ifndef TIM1PWM_H
+#define TIM1PWM_H
+
+void tim1pwm_config(void);
+
+
+#endif
---
+++ FOCyou/src/main.c
@@ -0,0 +1,12 @@
+#include <stm32f411xe.h>
+
+#include "tim1pwm.h"
+
+void GPIO_config(void);
+
+int main(void)
+{
+
+
+    return 0;
+}
