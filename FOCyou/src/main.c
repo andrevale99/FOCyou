5780ef5f7daf820338452e5566fcdc42da372532
andrevale99 <andrefvale@gmail.com>
2026-06-03 10:05:52 -0300

    .

--- FOCyou/src/main.c
+++ FOCyou/src/main.c
@@ -1,10 +1,14 @@
 #include <stm32f411xe.h>
 
-#include "systick.h"
-#include "lcd16x2.h"
-#include "lcd16x2_init.h"
+#include "init_systick.h"
+#include "init_lcd16x2.h"
+#include "init_inversor.h"
+#include "init_rcc.h"
 
-lcd16x2_handle handlelcd16x2 = {
+#include "lcd16x2.h"
+#include "inversor.h"
+
+const lcd16x2_handle handlelcd16x2 = {
     .d4.write = write_d4,
     .d5.write = write_d5,
     .d6.write = write_d6,
@@ -16,23 +20,33 @@ lcd16x2_handle handlelcd16x2 = {
     .delay_ms = delay_ms,
 };
 
+const inversor_t inversor = {
+    .timer = TIM1,
+    .periodo = 1249,
+    .prescale = 1,
+
+    .a.fase = 500,
+    .b.fase = 0,
+    .c.fase = 0,
+};
+
 int main(void)
 {
-    systick_init();
-    gpio_lcd16x2_setup();
-    lcd16x2_init_4bits(&handlelcd16x2);
+    
+    init_systick();
+    init_rcc();
 
-    char msg[] = {"teste"};
+    init_periferico_lcd16x2();
+    init_periferico_inversor();
+    
+    
+    lcd16x2_init_4bits(&handlelcd16x2);
+    inversor_init(&inversor);
+
 
     while (1)
     {
-        lcd16x2_write_string(&handlelcd16x2, msg, 3);
-        lcd16x2_send_cmd(&handlelcd16x2, RETURN_HOME);
-        delay_ms(1000);
-        lcd16x2_send_cmd(&handlelcd16x2, SHIFT_RIGHT);
-        delay_ms(1000);
-        // lcd16x2_send_cmd(&handlelcd16x2, RETURN_HOME);
-        // delay_ms(1000);
+
     }
 
     return 0;
