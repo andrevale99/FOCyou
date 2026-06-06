0f2ebbabb27de6535b555a033888ba90982005c2
andrevale99 <andrefvale@gmail.com>
2026-06-06 13:54:30 -0300

    Update

---
+++ FOCyou/include/macros.h
@@ -0,0 +1,16 @@
+#ifndef MACRO_H
+#define MACRO_H
+
+typedef struct
+{
+    float Ia;
+    float Ib;
+    
+    float Ialpha;
+    float Ibeta;
+
+    float Id;
+    float Iq;
+}foc_transform_t;
+
+#endif
