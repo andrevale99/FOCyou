3ae01880c27a73204b262cfc9f89fa2c2aa8f77c
andrevale99 <andrefvale@gmail.com>
2026-06-06 13:54:24 -0300

    Update

--- FOCyou/src/main.c
+++ FOCyou/src/main.c
@@ -1,14 +1,19 @@
+#include <stdio.h>
+
 #include <stm32f411xe.h>
 
+#include <arm_math.h>
+
+#include "macros.h"
+#include "init_rcc.h"
 #include "init_systick.h"
 #include "init_lcd16x2.h"
 #include "init_inversor.h"
-#include "init_rcc.h"
 
 #include "lcd16x2.h"
 #include "inversor.h"
 
-const lcd16x2_handle handlelcd16x2 = {
+const lcd16x2_handle lcd = {
     .d4.write = write_d4,
     .d5.write = write_d5,
     .d6.write = write_d6,
@@ -20,9 +25,9 @@ const lcd16x2_handle handlelcd16x2 = {
     .delay_ms = delay_ms,
 };
 
-const inversor_t inversor = {
+const inversor_t inv = {
     .timer = TIM1,
-    .periodo = 1249,
+    .periodo = 624,
     .prescale = 1,
 
     .a.fase = 500,
@@ -30,19 +35,27 @@ const inversor_t inversor = {
     .c.fase = 0,
 };
 
+foc_transform_t foc = {0};
+
 int main(void)
 {
     
-    init_systick();
     init_rcc();
+    init_systick();
 
     init_periferico_lcd16x2();
     init_periferico_inversor();
     
     
-    lcd16x2_init_4bits(&handlelcd16x2);
-    inversor_init(&inversor);
+    lcd16x2_init_4bits(&lcd);
+    lcd16x2_send_cmd(&lcd, DISPLAY_ON | CURSOR_ON);
 
+    inversor_init(&inv);
+
+    char buffer[16];
+
+    int size = sprintf(buffer, "SW:%ldkHz", inversor_frequencia_chaveamento(&inv,SystemCoreClock));
+    lcd16x2_write_string(&lcd, buffer, size);
 
     while (1)
     {
