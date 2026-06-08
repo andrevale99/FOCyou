39029667797034c0b8ac17803b4cf62ac5b1d979
andrevale99 <andrefvale@gmail.com>
2026-06-08 14:44:45 -0300

    update

--- FOCSTM32/src/main.c
+++ FOCSTM32/src/main.c
@@ -6,6 +6,7 @@
 
 #include "macros.h"
 #include "init_rcc.h"
+#include "init_adc.h"
 #include "init_systick.h"
 #include "init_lcd16x2.h"
 #include "init_inversor.h"
@@ -39,27 +40,42 @@ foc_transform_t foc = {0};
 
 int main(void)
 {
-    
+
     init_rcc();
     init_systick();
 
     init_periferico_lcd16x2();
     init_periferico_inversor();
-    
-    
+    init_periferico_adc();
+
     lcd16x2_init_4bits(&lcd);
     lcd16x2_send_cmd(&lcd, DISPLAY_ON | CURSOR_ON);
 
     inversor_init(&inv);
 
     char buffer[16];
-
-    int size = sprintf(buffer, "SW:%ldkHz", inversor_frequencia_chaveamento(&inv,SystemCoreClock));
-    lcd16x2_write_string(&lcd, buffer, size);
-
+    int size = 0;
     while (1)
     {
+        size = sprintf(buffer, "SW:%ldkHz", inversor_frequencia_chaveamento(&inv, SystemCoreClock));
+        lcd16x2_write_string(&lcd, buffer, size);
 
+        ADC1->CR2 |= ADC_CR2_SWSTART;
+        while (!(ADC1->SR & ADC_SR_EOC))
+            ;
+        uint16_t adc1 = ADC1->DR;
+
+        while (!(ADC1->SR & ADC_SR_EOC))
+            ;
+        uint16_t adc2 =  ADC1->DR;
+
+        lcd16x2_send_cmd(&lcd, SECOND_LINE | 0x01);
+        size = sprintf(buffer, "%d  %d ", adc1, adc2);
+        lcd16x2_write_string(&lcd, buffer, size);
+
+        lcd16x2_send_cmd(&lcd, RETURN_HOME);
+
+        delay_ms(2000);
     }
 
     return 0;
