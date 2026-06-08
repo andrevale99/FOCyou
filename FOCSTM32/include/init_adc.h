71a8429688dc18d75f85422f92b67960cbf720e8
andrevale99 <andrefvale@gmail.com>
2026-06-08 14:44:39 -0300

    ADC configurado para disparo manual

--- FOCSTM32/include/init_adc.h
+++ FOCSTM32/include/init_adc.h
@@ -3,6 +3,10 @@
 
 #include "libmcu.h"
 
+#define DMA_BUFFER_SIZE 2
+
+volatile uint16_t adcBuffer[DMA_BUFFER_SIZE];
+
 #define ADC_INPUT_1_GPIOA 6
 #define ADC_INPUT_2_GPIOA 7
 
@@ -10,6 +14,7 @@ void init_periferico_adc(void)
 {
     RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+    RCC->AHB1ENR |= RCC_AHB1ENR_DMA2EN;
 
     GPIOA->MODER &= ~((0x3 << (ADC_INPUT_1_GPIOA * 2)) |
                       (0x3 << (ADC_INPUT_2_GPIOA * 2)));
@@ -18,31 +23,28 @@ void init_periferico_adc(void)
                     (0x3 << (ADC_INPUT_2_GPIOA * 2));
 
     // Resolucao 12 bits, modo SCAN
-    ADC1->CR1 &= ~(0x3 << ADC_CR1_RES_Pos);
+    ADC1->CR1 &= ~ADC_CR1_RES;
     ADC1->CR1 |= ADC_CR1_SCAN;
 
-    // dado alinhado para a direita, DMA ativada
+    // dado alinhado para a direit
     ADC1->CR2 &= ~ADC_CR2_ALIGN;
     ADC1->CR2 &= ~(ADC_CR2_EXTEN | ADC_CR2_EXTSEL);
-    ADC1->CR2 |= (1U << ADC_CR2_EXTEN_Pos) | (0x9 << ADC_CR2_EXTSEL_Pos);
-    ADC1->CR2 |= ADC_CR2_DMA;
-    ADC1->CR2 |= ADC_CR2_DDS;
-
-    ADC1->SMPR2 &= ~(ADC_SMPR2_SMP6 | ADC_SMPR2_SMP7);
-
-    ADC1->SMPR2 |= (0x3U << ADC_SMPR2_SMP6_Pos) | (0x3U << ADC_SMPR2_SMP7_Pos);
+    // ADC1->CR2 |= (1U << ADC_CR2_EXTEN_Pos) | (0x9 << ADC_CR2_EXTSEL_Pos);
+    ADC1->CR2 |= ADC_CR2_EOCS;
 
     // 1 conversao
-    ADC1->SQR1 &= ~(0xF << ADC_SQR1_L_Pos);
+    ADC1->SQR1 &= ~ADC_SQR1_L;
     ADC1->SQR1 |= (1 << ADC_SQR1_L_Pos);
 
     // PA6 e PA7 nas convesoes
     ADC1->SQR3 &= ~(ADC_SQR3_SQ1 | ADC_SQR3_SQ2);
     ADC1->SQR3 |= (6 << ADC_SQR3_SQ1_Pos) | (7 << ADC_SQR3_SQ2_Pos);
 
-    ADC1->CR2 |= ADC_CR2_ADON;
+    ADC1->SMPR2 &= ~(ADC_SMPR2_SMP6 | ADC_SMPR2_SMP7);
 
-    //CONFIGURAR TIM1_CCR4 para estouro
+    ADC1->SMPR2 |= (0x3U << ADC_SMPR2_SMP6_Pos) | (0x3U << ADC_SMPR2_SMP7_Pos);
+
+    ADC1->CR2 |= ADC_CR2_ADON;
 }
 
 #endif
