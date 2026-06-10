4ae46a00667498a1a15b0f26c4bf3c72071e8db2
andrevale99 <andrefvale@gmail.com>
2026-06-10 10:20:09 -0300

    Refazendo a biblioteca do inversor

--- FOCYou/lib/inversor/inversor.c
+++ FOCYou/lib/inversor/inversor.c
@@ -1,44 +1,9 @@
 #include "inversor.h"
 
-int8_t inversor_init(const inversor_t *handle)
+void inversor_set_duty(inversor_t *inv_t,
+                       uint32_t duty_a, uint32_t duty_b, uint32_t duty_c)
 {
-    if (!handle)
-        return -1;
-
-    handle->timer->PSC = handle->prescale;
-    handle->timer->ARR = handle->periodo;
-
-    // center-aligned pwm up-down
-    handle->timer->CR1 &= ~TIM_CR1_CMS_Msk;
-    handle->timer->CR1 |= (3 << TIM_CR1_CMS_Pos);
-
-    handle->timer->CCMR1 |= (6 << TIM_CCMR1_OC1M_Pos);
-    handle->timer->CCMR1 |= TIM_CCMR1_OC1PE;
-
-    handle->timer->CCER |= (TIM_CCER_CC1E | TIM_CCER_CC1NE);
-
-    handle->timer->BDTR &= ~TIM_BDTR_DTG_Msk;
-    handle->timer->BDTR |= (200 << TIM_BDTR_DTG_Pos);
-
-    handle->timer->CCR1 = handle->a.fase;
-
-    /* Main output enable */
-    handle->timer->BDTR |= TIM_BDTR_MOE;
-
-    /* Update registers */
-    handle->timer->EGR |= TIM_EGR_UG;
-
-    /* Start timer */
-    handle->timer->CR1 |= TIM_CR1_CEN;
-
-    return 0;
-}
-
-uint32_t inversor_frequencia_chaveamento(const inversor_t *inv,
-                                         uint32_t sisClockHz)
-{
-    return (sisClockHz /
-           (2 * (inv->timer->PSC + 1) *
-            (inv->timer->ARR + 1)) /
-           1000);
+    inv_t->invTimer->advTimer->CCR1 = duty_a;
+    inv_t->invTimer->advTimer->CCR2 = duty_b;
+    inv_t->invTimer->advTimer->CCR3 = duty_c;
 }
