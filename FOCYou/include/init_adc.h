f9b9fba72a5aae8837afec488dbb40a804374095
andrevale99 <andrefvale@gmail.com>
2026-06-11 17:57:37 -0300

    ADC por disparo do canal 1

--- FOCYou/include/init_adc.h
+++ FOCYou/include/init_adc.h
@@ -3,18 +3,14 @@
 
 #include "libmcu.h"
 
-#define DMA_BUFFER_SIZE 2
-
-volatile uint16_t adcBuffer[DMA_BUFFER_SIZE];
-
 #define ADC_INPUT_1_GPIOA 6
 #define ADC_INPUT_2_GPIOA 7
 
 void init_periferico_adc(void)
 {
+
     RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-    RCC->AHB1ENR |= RCC_AHB1ENR_DMA2EN;
 
     GPIOA->MODER &= ~((0x3 << (ADC_INPUT_1_GPIOA * 2)) |
                       (0x3 << (ADC_INPUT_2_GPIOA * 2)));
@@ -22,27 +18,20 @@ void init_periferico_adc(void)
     GPIOA->MODER |= (0x3 << (ADC_INPUT_1_GPIOA * 2)) |
                     (0x3 << (ADC_INPUT_2_GPIOA * 2));
 
-    // Resolucao 12 bits, modo SCAN
-    ADC1->CR1 &= ~ADC_CR1_RES;
+    ADC1->CR1 = 0;
     ADC1->CR1 |= ADC_CR1_SCAN;
 
-    // dado alinhado para a direit
-    ADC1->CR2 &= ~ADC_CR2_ALIGN;
-    ADC1->CR2 &= ~(ADC_CR2_EXTEN | ADC_CR2_EXTSEL);
-    // ADC1->CR2 |= (1U << ADC_CR2_EXTEN_Pos) | (0x9 << ADC_CR2_EXTSEL_Pos);
-    ADC1->CR2 |= ADC_CR2_EOCS;
+    ADC1->CR2 = 0;
+    ADC1->CR2 |= ADC_CR2_EXTEN_0;
 
-    // 1 conversao
-    ADC1->SQR1 &= ~ADC_SQR1_L;
-    ADC1->SQR1 |= (1 << ADC_SQR1_L_Pos);
+    ADC1->SQR1 = 0;
+    ADC1->SQR2 = 0;
+    ADC1->SQR3 = 0;
 
-    // PA6 e PA7 nas convesoes
-    ADC1->SQR3 &= ~(ADC_SQR3_SQ1 | ADC_SQR3_SQ2);
-    ADC1->SQR3 |= (6 << ADC_SQR3_SQ1_Pos) | (7 << ADC_SQR3_SQ2_Pos);
+    ADC1->SQR1 |= (ADC_SQR1_L_0);
+    ADC1->SQR3 |= (6 << ADC_SQR3_SQ1_Pos) | ((7 << ADC_SQR3_SQ2_Pos));
 
-    ADC1->SMPR2 &= ~(ADC_SMPR2_SMP6 | ADC_SMPR2_SMP7);
-
-    ADC1->SMPR2 |= (0x3U << ADC_SMPR2_SMP6_Pos) | (0x3U << ADC_SMPR2_SMP7_Pos);
+    ADC1->SMPR2 = 0; //3 ciclos
 
     ADC1->CR2 |= ADC_CR2_ADON;
 }
